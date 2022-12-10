5f7966ab693d7be0c8c3a3b89fd06a04839d469f
PandoraLH <lixhen2002@gmail.com>
2022-12-10 15:25:07 +0700

    update pickrole

---
+++ src/pages/PickRolePage/PickRolePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './PickRolePage.scss'
+
+
+const PickRolePage = (props) => {
+  return <div className="PickRolePage">PickRolePage</div>
+}
+
+export default PickRolePage
--- src/routers/routerPath/publicPath.js
+++ src/routers/routerPath/publicPath.js
@@ -2,6 +2,7 @@ const PATH = {
     HOME_PAGE_PATH: '/home',
     LOGIN_PATH: '/login',
     SIGN_UP_PATH: '/signup',
+    PICKROLE_PATH: '/pickrole'
   }
   
   export default PATH
