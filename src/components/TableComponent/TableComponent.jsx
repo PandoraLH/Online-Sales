dc325c5ea0017fa8c10f3632bd184fa75dbae446
Quoc Dat <phanduongquocdat273@gmail.com>
2022-12-23 22:14:20 +0700

    final 1

--- src/components/TableComponent/TableComponent.jsx
+++ src/components/TableComponent/TableComponent.jsx
@@ -1,34 +1,15 @@
 import React, { useState } from "react";
 import "./TableComponent.scss";
 import { Table } from "antd";
-import { Pagination } from 'antd';
-
-const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-        console.log(
-            `selectedRowKeys: ${selectedRowKeys}`,
-            "selectedRows: ",
-            selectedRows
-        );
-    },
-    getCheckboxProps: (record) => ({
-        disabled: record.name === "Disabled User",
-        name: record.name,
-    }),
-};
 
 const TableComponent = ({ columns, data }) => {
     const [selectionType, setSelectionType] = useState("checkbox");
     return (
         <div className="TableComponent">
             <Table
-                rowSelection={{
-                    type: selectionType,
-                    ...rowSelection,
-                }}
                 columns={columns}
                 dataSource={data}
-                pagination={<Pagination defaultCurrent={1} defaultPageSize={5} />}
+                pagination={{ pageSize: 10 }}
             />
         </div>
     );
